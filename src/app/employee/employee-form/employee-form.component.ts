23561147605bd39a1bafd45e5617e6671f932ee1
edwinforcode <edwinjose.in@outlook.com>
2021-06-15 16:37:28 +0530

    basic setup

---
+++ src/app/employee/employee-form/employee-form.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'employee-form',
+  templateUrl: './employee-form.component.html'
+})
+export class EmployeeFormComponent {}
--- src/app/employee/employee.module.ts
+++ src/app/employee/employee.module.ts
@@ -1,11 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { EmployeeFormComponent } from './employee-form/employee-form.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { EmployeeRoutingModule } from './employee-routing.module';
 import { EmployeeComponent } from './employee.component';
 
 @NgModule({
   imports: [CommonModule, EmployeeRoutingModule],
   exports: [],
-  declarations: [EmployeeComponent]
+  declarations: [
+    EmployeeComponent,
+    EmployeeListComponent,
+    EmployeeFormComponent
+  ]
 })
 export class EmployeeModule {}
